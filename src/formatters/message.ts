e1c8c2ac87a6fdb7d6c47578f8f4167d1919995e
Julien AGUILAR <julien.aguilar6@gmail.com>
2019-09-16 10:22:40 +0200

    adds values in formatting fallback

--- src/formatters/message.ts
+++ src/formatters/message.ts
@@ -154,15 +154,25 @@ export function formatMessage(
       )
     );
     if (typeof message === 'string') {
-      return message || defaultMessage || id;
+      return (
+        message ||
+        defaultMessage ||
+        (hasValues ? `${id} ${JSON.stringify(values)}` : id)
+      );
     }
-    return defaultMessage || id;
+    return (
+      defaultMessage || (hasValues ? `${id} ${JSON.stringify(values)}` : id)
+    );
   }
   if (
     formattedMessageParts.length === 1 &&
     typeof formattedMessageParts[0] === 'string'
   ) {
-    return (formattedMessageParts[0] as string) || defaultMessage || id;
+    return (
+      (formattedMessageParts[0] as string) ||
+      defaultMessage ||
+      (hasValues ? `${id} ${JSON.stringify(values)}` : id)
+    );
   }
   return formattedMessageParts;
 }
